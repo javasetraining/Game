6bc7f6e4c2d55829a9a4405f299d711f39e4063d
Code <javasetraining@gmail.com>
2016-08-17 19:11:37 +0300

    AbstractException

--- src/io/hexlet/xo/model/Filed.java
+++ src/io/hexlet/xo/model/Filed.java
@@ -5,6 +5,8 @@ import java.awt.*;
 
 public class Filed {
     public static final int FIELD_SIZE = 3;
+    private static final int MIN_COORDINATE = 0;
+    private static final int MAX_COORDINATE = FIELD_SIZE;
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
     public int getSize() {
@@ -20,4 +22,11 @@ public class Filed {
 
     }
 
+    private boolean checkPoint(final Point point) {
+        return checkCootdinate(point.x) && checkCootdinate(point.y);
+    }
+
+    private boolean checkCootdinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
+    }
 }
---
+++ src/io/hexlet/xo/model/exception/AbstractXOException.java
@@ -0,0 +1,7 @@
+package io.hexlet.xo.model.exception;
+
+/**
+ * Created by MaRV on 8/17/2016.
+ */
+public abstract class AbstractXOException extends Exception{
+}
