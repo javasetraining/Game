5ed4a96cb4cd4a3bb0f7d78e870d6b418353dc4f
Code <javasetraining@gmail.com>
2016-08-17 21:26:38 +0300

    innitial move controller added

--- src/io/hexlet/xo/controllers/MoveController.java
+++ src/io/hexlet/xo/controllers/MoveController.java
@@ -1,10 +1,19 @@
 package io.hexlet.xo.controllers;
 
 
+import io.hexlet.xo.model.Fieled;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exception.AlreadyOccupiedException;
+import io.hexlet.xo.model.exception.InvalidPointException;
+
+import java.awt.*;
+import java.lang.reflect.Field;
+
 public class MoveController {
-
-
-    public void applyFigure(){
-
+    public void applyFigure(Fieled fieled, Point point, Figure figure) throws InvalidPointException, AlreadyOccupiedException {
+        if (fieled.getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
+        fieled.setFigure(point, figure);
     }
 }
